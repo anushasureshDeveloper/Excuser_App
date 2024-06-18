5904da2511d942fb0f304acc3c7cec775fdeeee2
anushasureshDeveloper <anushasuresh2323@gmail.com>
2024-06-18 12:51:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios"
+import { useState } from "react";
+function App (){
+const[excuse,setExcuse]=useState("")
+    const fetchData=async(reason)=>{
+const {data}=await axios.get(`https://excuser-three.vercel.app/v1/excuse/${reason}`)
+setExcuse(data[0].excuse)
+
+    }
+    return(
+      
+
+        <div className="flex flex-col justify-center items-center gap-10 bg-black min-h-screen text-blue-500 px-6 py-2"> 
+          <button onClick={()=>{fetchData("funny")}}  className="flex   text-sky-600 text-xl bg-cyan-100 px-4 py-2 font-bold" >Funny</button>
+            <button onClick={()=>{fetchData("developers")}} className="flex   text-blue-500 text-xl  bg-cyan-100 px-4 py-2 font-bold" >Developer</button>
+            <button onClick={()=>{fetchData("office")}} className="flex  text-cyan-600 text-xl bg-cyan-100 px-6 py-2 font-bold" >Office</button>
+            <h1 className="text-xl">{excuse} </h1>
+          </div>
+    )
 }
-
-export default App;
+export default App   
